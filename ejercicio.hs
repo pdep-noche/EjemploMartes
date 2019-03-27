55c3e065d56480fd3a942605a36793de6279dd35
vpennella <vpennella@gmail.com>
2019-03-26 21:50:01 -0300

    nuevo ejercicio

---
+++ ejercicio.hs
@@ -0,0 +1,32 @@
+siguiente nro = nro +1
+
+
+data Empleado = Comun {sueldoBasico :: Double, nombre:: String} | Jefe { sueldo :: Double, cantACargo:: Double, nombre:: String} deriving Show
+
+sueldoTotal :: Empleado -> Double
+sueldoTotal (Comun sueldo _) = sueldo
+sueldoTotal (Jefe sueldo cantidad _) = sueldo + plus cantidad
+
+plus cantidad = cantidad * 500
+
+analia = Comun 10000 "analia"
+juan = Jefe 15000 3 "juan"
+
+and' :: Bool -> Bool -> Bool
+and' unVal otroVal | unVal = otroVal
+                   | otherwise = False
+
+and'' :: Bool -> Bool -> Bool
+and'' True val = val
+and'' _ _ = False
+
+or' :: Bool -> Bool -> Bool
+or' False val = val
+or' _ _ = True
+
+or'' :: Bool -> Bool -> Bool
+or'' True _ = True
+or'' _ val = val
+
+
+
